c4663268e64ae12397e09f6ba093c81a4c4f666e
bouatim el mahfoud <bouatimelmahfoud@gmail.com>
2019-12-18 02:59:05 +0100

    Updates

---
+++ precache-manifest.52663754b04d5f22734cf9d44de29622.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "372dd280a4a9c9f02336d949258489a2",
+    "url": "/iot/index.html"
+  },
+  {
+    "revision": "94a2b9059ed58b4abdd2",
+    "url": "/iot/static/css/main.1a83c786.chunk.css"
+  },
+  {
+    "revision": "2c41fa3599ce50e164a9",
+    "url": "/iot/static/js/2.2975e583.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/iot/static/js/2.2975e583.chunk.js.LICENSE"
+  },
+  {
+    "revision": "94a2b9059ed58b4abdd2",
+    "url": "/iot/static/js/main.c4f2306a.chunk.js"
+  },
+  {
+    "revision": "9e87c6f42311be433910",
+    "url": "/iot/static/js/runtime-main.16ba1076.js"
+  },
+  {
+    "revision": "f8759fa3b63d8ca8419e3b234f76fae4",
+    "url": "/iot/static/media/webreathe.f8759fa3.png"
+  }
+]);
