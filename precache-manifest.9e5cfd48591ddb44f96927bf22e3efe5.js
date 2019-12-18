b1f6438af47143a777abe612b071e8f7942cf4ce
bouatim el mahfoud <bouatimelmahfoud@gmail.com>
2019-12-18 03:04:19 +0100

    Updates

---
+++ precache-manifest.9e5cfd48591ddb44f96927bf22e3efe5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecf05dd26e6ef496e14bd5b102b7202d",
+    "url": "/webreathe/index.html"
+  },
+  {
+    "revision": "f697af0d20e903ff5477",
+    "url": "/webreathe/static/css/main.1a83c786.chunk.css"
+  },
+  {
+    "revision": "2c41fa3599ce50e164a9",
+    "url": "/webreathe/static/js/2.2975e583.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/webreathe/static/js/2.2975e583.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f697af0d20e903ff5477",
+    "url": "/webreathe/static/js/main.591f976e.chunk.js"
+  },
+  {
+    "revision": "c245b4950b1902766902",
+    "url": "/webreathe/static/js/runtime-main.c80820c6.js"
+  },
+  {
+    "revision": "f8759fa3b63d8ca8419e3b234f76fae4",
+    "url": "/webreathe/static/media/webreathe.f8759fa3.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/iot/precache-manifest.52663754b04d5f22734cf9d44de29622.js"
+  "/webreathe/precache-manifest.9e5cfd48591ddb44f96927bf22e3efe5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/iot/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/webreathe/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
