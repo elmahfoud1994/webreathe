750c25774967053d4d8b3266f2c2d01909d510cc
bouatim el mahfoud <bouatimelmahfoud@gmail.com>
2019-12-25 00:27:00 +0100

    Updates

---
+++ precache-manifest.ee7ab71946fc104a0320010094f7668f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e60afb86624aa83eb86e75fc3ba164e5",
+    "url": "/webreathe/index.html"
+  },
+  {
+    "revision": "d024ae6d0c9c0e7ee8af",
+    "url": "/webreathe/static/css/main.8980c075.chunk.css"
+  },
+  {
+    "revision": "185cbbed2fb31956c66e",
+    "url": "/webreathe/static/js/2.2146bd76.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/webreathe/static/js/2.2146bd76.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d024ae6d0c9c0e7ee8af",
+    "url": "/webreathe/static/js/main.d52f01b0.chunk.js"
+  },
+  {
+    "revision": "c245b4950b1902766902",
+    "url": "/webreathe/static/js/runtime-main.c80820c6.js"
+  },
+  {
+    "revision": "4021742975cad3e2139046b4b7179d29",
+    "url": "/webreathe/static/media/iot.40217429.png"
+  },
+  {
+    "revision": "f8759fa3b63d8ca8419e3b234f76fae4",
+    "url": "/webreathe/static/media/webreathe.f8759fa3.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d7485f40e30568b68c178a6ae68f6361.js"
+  "/webreathe/precache-manifest.ee7ab71946fc104a0320010094f7668f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/webreathe/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
