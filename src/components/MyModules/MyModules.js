3eecc18feb0fefb185ef456605309ce5e98dc9bc
bouatim el mahfoud <bouatimelmahfoud@gmail.com>
2019-12-25 23:45:02 +0100

    'hhhh'

--- src/components/MyModules/MyModules.js
+++ src/components/MyModules/MyModules.js
@@ -2,7 +2,6 @@ import React,{Component} from 'react'
 import './MyModules.css'
 import MyModule from './MyModule/MyModule'
 import iotImage from '../../assets/images/iot.png'
-import PaginationItems from './PaginationItems/PaginationItems'
 class MyModules extends Component{
 	state={
 		modules:[
@@ -26,7 +25,7 @@ class MyModules extends Component{
 					this.state.modules.map(module=><MyModule description={module.description} title={module.title} createdAt={module.createdAt}/>)
 				}
 				</div>
-				<PaginationItems count={5}/>
+				
 			</div>
 		)
 	}
