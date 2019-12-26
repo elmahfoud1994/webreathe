dc85c8cb3dc3e9b66c01e0c5c89a8889aa4dc258
bouatim el mahfoud <bouatimelmahfoud@gmail.com>
2019-12-26 23:04:43 +0100

    'this'

--- src/components/Notifications/Notifications.js
+++ src/components/Notifications/Notifications.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './Notifications.css'
 const Notifications=(props)=>(
 	<div className="Notifications">
-		<h2>Notifications1</h2>
+		<h2>Notifications11</h2>
 	</div>
 )
 export default Notifications
