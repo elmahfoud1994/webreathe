7bd5de3161309afa3eaaf1a2f67c21614514f223
bouatim el mahfoud <bouatimelmahfoud@gmail.com>
2019-12-29 21:40:20 +0100

    hello

--- src/components/MyModuleDetails/MyModuleDetails.js
+++ src/components/MyModuleDetails/MyModuleDetails.js
@@ -6,7 +6,7 @@ import {MdDelete} from 'react-icons/md'
 const myModuleDetails=(props)=>(
 	<div className="MyModuleDetails">
 		<ModuleInfos title="New device" type="Smart tv" createdAt='02/12/1994 14:20' description="this is a smart tv that allows users to wach their best tv shows while sleeping" />
-		<h6>fdkjsdkfd</h6>
+		
 		<div className="MyModuleStats">
 			<ModuleMonitor/>
 		</div>
