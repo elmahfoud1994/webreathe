9f1cfa6870062abae3f6442d9e1dfb23da952312
bouatim el mahfoud <bouatimelmahfoud@gmail.com>
2020-01-05 18:48:51 +0100

    'loading-feature

--- src/components/MyModuleDetails/ModuleMonitor/ModuleMonitor.js
+++ src/components/MyModuleDetails/ModuleMonitor/ModuleMonitor.js
@@ -4,10 +4,11 @@ import ActivityDurationMonitor from './ActivityDurationMonitor/ActivityDurationM
 import ActivityStateMonitor from './ActivityStateMonitor/ActivityStateMonitor'
 import DataMonitor from './DataMonitor/DataMonitor'
 import TemperatureMonitor from './TemperatureMonitor/TemperatureMonitor'
-
+import LoadSprint2 from '../../ui/LoadSprint2/LoadSprint2'
 
 const moduleMonitor=(props)=>(
 	<div className="MyModuleMonitor">
+		{props.loading?<LoadSprint2 />:null}
 		{props.shouldMonitorDuration ? <ActivityDurationMonitor durationList={props.activityStateHistory}  /> :null }
 		{props.activityStateHistory.length ? <ActivityStateMonitor activityStateList={props.activityStateHistory}/> : null}
 		{props.temperatureHistory.length ? <TemperatureMonitor temperatureList={props.temperatureHistory}/> :null }
