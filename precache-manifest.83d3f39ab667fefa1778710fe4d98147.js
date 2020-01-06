72e742bb5297a039a22aa1f6e99ae55bc5d4029a
bouatim el mahfoud <bouatimelmahfoud@gmail.com>
2020-01-06 09:00:26 +0100

    Updates

---
+++ precache-manifest.83d3f39ab667fefa1778710fe4d98147.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f94a2c1940d028aae31db8cda3635a69",
+    "url": "/webreathe/index.html"
+  },
+  {
+    "revision": "6d761f2906d5a2471e08",
+    "url": "/webreathe/static/css/main.a5444d6a.chunk.css"
+  },
+  {
+    "revision": "246373e623b9ced00401",
+    "url": "/webreathe/static/js/2.3adf8ecd.chunk.js"
+  },
+  {
+    "revision": "44bb13c376a360a26522c8368cf779a1",
+    "url": "/webreathe/static/js/2.3adf8ecd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6d761f2906d5a2471e08",
+    "url": "/webreathe/static/js/main.d3707140.chunk.js"
+  },
+  {
+    "revision": "c245b4950b1902766902",
+    "url": "/webreathe/static/js/runtime-main.c80820c6.js"
+  },
+  {
+    "revision": "4799c96fc30525cbddb6e101c519ee87",
+    "url": "/webreathe/static/media/Error.4799c96f.svg"
+  },
+  {
+    "revision": "4021742975cad3e2139046b4b7179d29",
+    "url": "/webreathe/static/media/iot.40217429.png"
+  },
+  {
+    "revision": "e3381599830a064a7d25caeed015fd20",
+    "url": "/webreathe/static/media/moduleInfos.e3381599.svg"
+  },
+  {
+    "revision": "2a8153ef719d8cb58b576262b1e181e3",
+    "url": "/webreathe/static/media/notification.2a8153ef.svg"
+  },
+  {
+    "revision": "99bbd081d670924e2b5be9eb2e6fc3dd",
+    "url": "/webreathe/static/media/notification1.99bbd081.svg"
+  },
+  {
+    "revision": "f8759fa3b63d8ca8419e3b234f76fae4",
+    "url": "/webreathe/static/media/webreathe.f8759fa3.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/webreathe/precache-manifest.7df770eee3a848407fc7d705738369b7.js"
+  "/webreathe/precache-manifest.83d3f39ab667fefa1778710fe4d98147.js"
 );
 
 self.addEventListener('message', (event) => {
